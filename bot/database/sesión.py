1dc61eba03465988f32290b8504a4f29b39b1c4a
happydeath766-hue <happydeath.766@gmail.com>
2026-07-19 05:30:30 -0500

    Create sesión.py

---
+++ bot/database/sesión.py
@@ -0,0 +1,24 @@
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    AsyncSession,
+)
+
+from bot.config.config import settings
+
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=False,
+    future=True,
+)
+
+SessionLocal = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=False,
+    class_=AsyncSession,
+)
+
+
+async def get_session():
+    async with SessionLocal() as session:
+        yield session
