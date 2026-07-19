513ea0476aca50e92932dccccaeded5498a99e7f
happydeath766-hue <happydeath.766@gmail.com>
2026-07-19 05:47:38 -0500

    Update group_events.py

--- bot/handlers/group_events.py
+++ bot/handlers/group_events.py
@@ -1,21 +1,50 @@
 from aiogram import Router
 from aiogram.filters import ChatMemberUpdatedFilter
-from aiogram.types import ChatMemberUpdated
 from aiogram.enums import ChatMemberStatus
+from aiogram.types import ChatMemberUpdated
+
+from bot.config.config import settings
+from bot.keyboards.admin import pending_group_keyboard
 
 router = Router()
 
 
-@router.my_chat_member(
-    ChatMemberUpdatedFilter(
-        member_status_changed=(
-            ChatMemberStatus.LEFT >> ChatMemberStatus.MEMBER
+@router.my_chat_member()
+async def group_update(event: ChatMemberUpdated):
+
+    old = event.old_chat_member.status
+    new = event.new_chat_member.status
+
+    if old == ChatMemberStatus.LEFT and new in (
+        ChatMemberStatus.MEMBER,
+        ChatMemberStatus.ADMINISTRATOR,
+    ):
+
+        chat = event.chat
+
+        await event.bot.send_message(
+            settings.OWNER_ID,
+            f"""
+📥 Nuevo grupo detectado
+
+📛 Nombre:
+{chat.title}
+
+🆔
+<code>{chat.id}</code>
+
+Estado:
+⏳ Pendiente
+""",
+            reply_markup=pending_group_keyboard(chat.id)
         )
-    )
-)
-async def bot_added(event: ChatMemberUpdated):
-    chat = event.chat
 
-    print(f"Nuevo grupo: {chat.title}")
+    elif (
+        old in (
+            ChatMemberStatus.MEMBER,
+            ChatMemberStatus.ADMINISTRATOR,
+        )
+        and new == ChatMemberStatus.LEFT
+    ):
 
-    # Aquí después guardaremos el grupo en PostgreSQL
+        print("Bot eliminado del grupo")
