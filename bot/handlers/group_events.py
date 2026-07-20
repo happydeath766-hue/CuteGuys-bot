1319bb6a5c8016e0df8b3f4e4c83fda622e950fc
happydeath766-hue <happydeath.766@gmail.com>
2026-07-19 22:14:09 -0500

    Update group_events.py

--- bot/handlers/group_events.py
+++ bot/handlers/group_events.py
@@ -5,21 +5,32 @@ from aiogram.types import ChatMemberUpdated
 from bot.config.config import settings
 from bot.keyboards.admin import pending_group_keyboard
 from bot.services.group_service import GroupService
+
 router = Router()
 
 
-@router.my_chat_member()
+@router.my_chat_member(ChatMemberUpdatedFilter())
 async def group_update(event: ChatMemberUpdated):
+
+    print("🔥 Evento my_chat_member recibido")
 
     old = event.old_chat_member.status
     new = event.new_chat_member.status
 
-    if old == ChatMemberStatus.LEFT and new in (
-        ChatMemberStatus.MEMBER,
-        ChatMemberStatus.ADMINISTRATOR,
+    print(f"Estado anterior: {old}")
+    print(f"Estado nuevo: {new}")
+
+    if (
+        old == ChatMemberStatus.LEFT
+        and new in (
+            ChatMemberStatus.MEMBER,
+            ChatMemberStatus.ADMINISTRATOR,
+        )
     ):
 
         chat = event.chat
+
+        print(f"✅ Bot agregado a: {chat.title}")
 
         await GroupService.add_group(
             chat.id,
@@ -29,12 +40,31 @@ async def group_update(event: ChatMemberUpdated):
         )
 
         await event.bot.send_message(
-            settings.OWNER_ID,
-            f"""
+            chat_id=settings.OWNER_ID,
+            text=f"""
 📥 Nuevo grupo detectado
 
 📛 Nombre:
 {chat.title}
+
+🆔
+<code>{chat.id}</code>
+
+Estado:
+⏳ Pendiente
+""",
+            reply_markup=pending_group_keyboard(chat.id),
+        )
+
+    elif (
+        old in (
+            ChatMemberStatus.MEMBER,
+            ChatMemberStatus.ADMINISTRATOR,
+        )
+        and new == ChatMemberStatus.LEFT
+    ):
+
+        print(f"❌ Bot eliminado de: {event.chat.title}"){chat.title}
 
 🆔
 <code>{chat.id}</code>
